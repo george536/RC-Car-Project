1624dfaab73a30ddb5e98c6685992774701e687d
AmaanK2 <khakianiamaan@gmail.com>
2023-02-09 22:18:33 -0500

    mqtt server test

---
+++ connectivity_test.py
@@ -0,0 +1,91 @@
+import time
+import unittest
+
+from topics import Topic
+from message import MQTTMessage
+from mqtt_client import MQTTClient
+
+
+# Unit test for MQTT Communication test
+class MyTestCase(unittest.TestCase):
+
+    """
+    This test case tests a simple connection between one client and another. It simulates one vehicle publishing
+    its own lead detection data to the MQTT broker/server and another vehicle receiving it. We confirm proper
+    receiving at the end
+    """
+    def test_connection(self):
+
+        # create 2 vehicles
+        client_1 = MQTTClient(client_id='VEHICLE-1')
+
+        client_2 = MQTTClient(client_id='VEHICLE-2')
+
+        # connect both of them
+        client_1.connect()
+
+        client_2.connect()
+
+        # subscribe the first vehicle to lead detection topic
+
+        client_1.subscribe(topic=Topic.LEAD_DET)
+
+        # second vehicle publishes message to lead detection topic
+        # (Note: Do not need to sub to publish message on topic)
+
+        client_2.send_message(MQTTMessage(topic=Topic.LEAD_DET, message='Speed, 50'))
+
+        time.sleep(3)  # give time for message to arrive (this is too high, want to test with lower time).
+
+        self.assertEqual('Speed, 50', client_1.last_message)
+
+    """
+    Test case is intended to be used in conjunction with test_connection_subscriber_1. The two test cases in combination 
+    try to simulate a one to many communication scenario instead of simple 1-1. Many subscribing vehicles are
+    simulated in test_connection_subscriber_1. We run that test first and wait till all vehicles successfully connect
+    to the server. Then we run test_connection_publish_1 to send a message to the TEST topic. If successful all vehicles
+    in test_connection_subscriber_1 should receive the message published from this unit test. Additionally, we also
+    confirm to see if the test successfully published the message at the end. This helps with figuring out where
+    any issue is happening.
+    """
+    def test_connection_publish_1(self):
+
+        # create publishing vehicle and connect it
+        publish_vehicle = MQTTClient(client_id='VEHICLE-1')
+
+        publish_vehicle.connect()
+
+        # send message and confirm if it was sent
+        publish_vehicle.send_message(MQTTMessage(topic=Topic.TEST, message='Speed-50'))
+
+        time.sleep(5)
+        self.assertEqual(True, publish_vehicle.last_publish_successful)
+
+    def test_connection_subscriber_1(self):
+
+        NUM_SUBSCRIBERS = 5  # Control the number of receiving vehicles simulated
+
+        subscribers = []
+
+        # Create, connect and subscribe all receiving vehicles
+        for i in range(NUM_SUBSCRIBERS):
+            sub_vehicle = MQTTClient(client_id='VEHICLE-' + str(i + 2))
+            sub_vehicle.connect()
+            sub_vehicle.subscribe(topic=Topic.TEST)
+
+            subscribers.append(sub_vehicle)
+
+        print('Ready to receive')
+
+        time.sleep(30) # Wait for message to arrive (This time is too high, need to find lower bound)
+
+        # Confirm message is received by all subscribers (Run test_connection_publish_1)
+        for sub in subscribers:
+            self.assertEqual('Speed-50', sub.last_message)
+            print(sub.client_id, ' OK')
+
+        print('All OK')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ mqtt_client.py
@@ -0,0 +1,87 @@
+from base_client import Client
+from topics import *
+from message import MQTTMessage
+
+import paho.mqtt.client as paho
+from paho import mqtt
+
+import logging
+import os
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+# Details for MQTT HiveMQ server
+USER_NAME = 'mcmaster'
+PASSWORD = 'McMaster123'
+SERVER_URL = '5a3cea1ebc484e859be394be5b862daf.s2.eu.hivemq.cloud'
+SERVER_PORT = 8883
+
+WEBSOCKET_PORT = 8884
+
+DEBUG = True
+
+
+class MQTTClient(Client):
+    """
+    Provides base implementation for an MQTT Client. Extends Client class
+    We can connect to an MQTT server, subscribe, send and publish messages for topics
+    using this class.
+    """
+
+    def __init__(self, client_id):
+        Client.__init__(self, client_id)
+
+        self.logger = logging.getLogger(__name__)
+
+        self.client = paho.Client(client_id=client_id, userdata=None, protocol=paho.MQTTv5)
+        self.last_message = ''
+        self.last_publish_successful = False
+
+    def on_connect(self, client, userdata, flags, rc, properties=None):
+        if DEBUG:
+            print(self.client_id, "CONNACK received with code %s." % rc)
+
+    # with this callback you can see if your publish was successful
+    def on_publish(self, client, userdata, mid, properties=None):
+        if DEBUG:
+            print(self.client_id, 'Successfully published message')
+            self.last_publish_successful = True
+
+    # print which topic was subscribed to
+    def on_subscribe(self, client, userdata, mid, granted_qos, properties=None):
+        if DEBUG:
+            print(self.client_id, "Subscribed: " + str(mid) + " " + str(granted_qos))
+
+    # print message, useful for checking if it was successful
+    def on_message(self, client, userdata, msg):
+        message = MQTTMessage(topic=convert_string_topic(str(msg.topic)), message=str(msg.payload)[2:-1])
+        self.receive_message(message)
+
+        if DEBUG:
+            print(self.client_id, msg.topic + " " + str(msg.payload))
+
+    def connect(self):
+
+        # we are using SSL encryption to connect
+        self.client.on_connect = self.on_connect
+        self.client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+
+        self.client.username_pw_set(username=USER_NAME, password=PASSWORD)
+
+        # connect and start non-blocking loop. See Paho MQTT docs for details
+        self.client.connect(SERVER_URL, SERVER_PORT)
+        self.client.loop_start()
+
+        # set callback functions
+        self.client.on_subscribe = self.on_subscribe
+        self.client.on_message = self.on_message
+        self.client.on_publish = self.on_publish
+
+    def subscribe(self, topic: Topic, qos=1):
+        self.client.subscribe(convert_topic_string(topic), qos=qos)
+
+    def send_message(self, message: MQTTMessage, qos=1):
+        self.client.publish(message.get_topic_str(), message.get_message(), qos=qos)
+
+    def receive_message(self, message: MQTTMessage):
+        self.last_message = message.get_message()
