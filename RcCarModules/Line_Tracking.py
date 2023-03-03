779a097b2af569c5b3e680b547b5e88597dd3898
George Yazji <76963708+george536@users.noreply.github.com>
2023-03-02 19:55:32 -0500

    coeff

--- RcCarModules/Line_Tracking.py
+++ RcCarModules/Line_Tracking.py
@@ -31,8 +31,8 @@ class Line_Tracking:
 
         boundry2 = int(boundry2)
 
-        boundry1 = 1500
-        boundry2 = 2500
+        boundry1 = 1000
+        boundry2 = 2000
 
         self.LMR=0x00
         if GPIO.input(self.IR01)==True:
