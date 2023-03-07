a7cee81654041a1e0e09871c9aad27cf6727afd0
car1 <car1@example.com>
2023-03-07 22:21:38 +0000

    tape detecting done

--- main.py
+++ main.py
@@ -2,12 +2,18 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import cv2
 import numpy as np
 
+from picamera2 import Picamera2
+
 def nothing(x):
     pass
+
+picam2 = Picamera2()
+picam2.start()
+
+
 
 cv2.namedWindow('image')
 
@@ -30,11 +36,10 @@ phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-    image_source = cv2.VideoCapture(0)
+        
 
     while True:
-        ret, img = image_source.read()
+        img = picam2.capture_array()
 
         # get current positions of all trackbars
         hMin = cv2.getTrackbarPos('HMin', 'image')
@@ -46,19 +51,33 @@ if __name__ == '__main__':
         vMax = cv2.getTrackbarPos('VMax', 'image')
 
         # Set minimum and max HSV values to display
-        lower = np.array([hMin, sMin, vMin])
-        upper = np.array([hMax, sMax, vMax])
+        lower = np.array([30, 44, 202])
+        upper = np.array([57, 171, 255])
 
         # Create HSV Image and threshold into a range.
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower, upper)
-        output = cv2.bitwise_and(img, img, mask=mask)
+        
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(img, contours, -1, (0,255,0), 3)
+        
+        cnt = contours[0]
+        M = cv2.moments(cnt)
+        moment_0 = M['m00']
+        
+        if moment_0 == 0:
+            moment_0 = 1
+        cx = int(M['m10'] / moment_0)
+        cy = int(M['m01'] / moment_0)
 
-        cv2.imshow('image', mask)
+        cv2.circle(img, (cx, cy), 1, (0, 0, 255), 3)
+
+        cv2.imshow('image', img)
         # Wait longer to prevent freeze for videos.
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
+    video_cap.release()
     cv2.destroyAllWindows()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
