da1ef523e4a28e102a32187dcd722b80a68c6de9
George Yazji <76963708+george536@users.noreply.github.com>
2023-03-09 00:26:26 -0500

    trying to resize window

--- main.py
+++ main.py
@@ -16,19 +16,20 @@ picam2.start()
 
 
 cv2.namedWindow('image')
+cv2.resizeWindow('image', 640, 480)
 
-# create trackbars for color change
-cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
-cv2.createTrackbar('SMin','image',0,255,nothing)
-cv2.createTrackbar('VMin','image',0,255,nothing)
-cv2.createTrackbar('HMax','image',0,179,nothing)
-cv2.createTrackbar('SMax','image',0,255,nothing)
-cv2.createTrackbar('VMax','image',0,255,nothing)
+# # create trackbars for color change
+# cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+# cv2.createTrackbar('SMin','image',0,255,nothing)
+# cv2.createTrackbar('VMin','image',0,255,nothing)
+# cv2.createTrackbar('HMax','image',0,179,nothing)
+# cv2.createTrackbar('SMax','image',0,255,nothing)
+# cv2.createTrackbar('VMax','image',0,255,nothing)
 
-# Set default value for MAX HSV trackbars.
-cv2.setTrackbarPos('HMax', 'image', 179)
-cv2.setTrackbarPos('SMax', 'image', 255)
-cv2.setTrackbarPos('VMax', 'image', 255)
+# # Set default value for MAX HSV trackbars.
+# cv2.setTrackbarPos('HMax', 'image', 179)
+# cv2.setTrackbarPos('SMax', 'image', 255)
+# cv2.setTrackbarPos('VMax', 'image', 255)
 
 # Initialize to check if HSV min/max value changes
 hMin = sMin = vMin = hMax = sMax = vMax = 0
@@ -41,14 +42,14 @@ if __name__ == '__main__':
     while True:
         img = picam2.capture_array()
 
-        # get current positions of all trackbars
-        hMin = cv2.getTrackbarPos('HMin', 'image')
-        sMin = cv2.getTrackbarPos('SMin', 'image')
-        vMin = cv2.getTrackbarPos('VMin', 'image')
+        # # get current positions of all trackbars
+        # hMin = cv2.getTrackbarPos('HMin', 'image')
+        # sMin = cv2.getTrackbarPos('SMin', 'image')
+        # vMin = cv2.getTrackbarPos('VMin', 'image')
 
-        hMax = cv2.getTrackbarPos('HMax', 'image')
-        sMax = cv2.getTrackbarPos('SMax', 'image')
-        vMax = cv2.getTrackbarPos('VMax', 'image')
+        # hMax = cv2.getTrackbarPos('HMax', 'image')
+        # sMax = cv2.getTrackbarPos('SMax', 'image')
+        # vMax = cv2.getTrackbarPos('VMax', 'image')
 
         # Set minimum and max HSV values to display
         lower = np.array([30, 44, 202])
