e1bc64324cd01503a4add915d1e82b90b15f1e3f
George Yazji <76963708+george536@users.noreply.github.com>
2023-03-12 18:30:22 -0400

    cc

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ picam2.start()
 cv2.namedWindow('image')
 
 # # create trackbars for color change
-# cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
-# cv2.createTrackbar('SMin','image',0,255,nothing)
-# cv2.createTrackbar('VMin','image',0,255,nothing)
-# cv2.createTrackbar('HMax','image',0,179,nothing)
-# cv2.createTrackbar('SMax','image',0,255,nothing)
-# cv2.createTrackbar('VMax','image',0,255,nothing)
+cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+cv2.createTrackbar('SMin','image',0,255,nothing)
+cv2.createTrackbar('VMin','image',0,255,nothing)
+cv2.createTrackbar('HMax','image',0,179,nothing)
+cv2.createTrackbar('SMax','image',0,255,nothing)
+cv2.createTrackbar('VMax','image',0,255,nothing)
 
-# # Set default value for MAX HSV trackbars.
-# cv2.setTrackbarPos('HMax', 'image', 179)
-# cv2.setTrackbarPos('SMax', 'image', 255)
-# cv2.setTrackbarPos('VMax', 'image', 255)
+# Set default value for MAX HSV trackbars.
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
 
 # Initialize to check if HSV min/max value changes
 hMin = sMin = vMin = hMax = sMax = vMax = 0
