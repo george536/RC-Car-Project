f32b360d7df7329b6db821d29e9d000e752ebcde
George Yazji <76963708+george536@users.noreply.github.com>
2023-03-12 20:08:37 -0400

    added calibration

---
+++ calibrate.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import json
+from picamera2 import Picamera2
+
+def nothing(x):
+    pass
+
+picam2 = Picamera2()
+picam2.start()
+
+
+cv2.namedWindow('image')
+
+# # create trackbars for color change
+cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+cv2.createTrackbar('SMin','image',0,255,nothing)
+cv2.createTrackbar('VMin','image',0,255,nothing)
+cv2.createTrackbar('HMax','image',0,179,nothing)
+cv2.createTrackbar('SMax','image',0,255,nothing)
+cv2.createTrackbar('VMax','image',0,255,nothing)
+
+# Set default value for MAX HSV trackbars.
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize to check if HSV min/max value changes
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    with open('cameraParameters.json', 'r') as file:
+        data = json.load(file)
+
+    while True:
+        img = picam2.capture_array()
+        img = img[240:,:]
+
+        # # get current positions of all trackbars
+        hMin = cv2.getTrackbarPos('HMin', 'image')
+        sMin = cv2.getTrackbarPos('SMin', 'image')
+        vMin = cv2.getTrackbarPos('VMin', 'image')
+
+        hMax = cv2.getTrackbarPos('HMax', 'image')
+        sMax = cv2.getTrackbarPos('SMax', 'image')
+        vMax = cv2.getTrackbarPos('VMax', 'image')
+
+        # Set minimum and max HSV values to display
+        lower = np.array([hMin, sMin, vMin])
+        upper = np.array([hMax, sMax, vMax])
+
+        data['hMin'] = hMin
+        data['sMin'] = sMin
+        data['vMin'] = vMin
+        data['hMax'] = hMax
+        data['sMax'] = sMax
+        data['vMax'] = vMax
+
+        with open('cameraParameters.json', 'w') as file:
+            json.dump(data, file)
+        
+
+        # Create HSV Image and threshold into a range.
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+        
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(img, contours, -1, (0,255,0), 3)
+        
+        if len(contours)==0:
+            continue
+        cnt = contours[0]
+        M = cv2.moments(cnt)
+        moment_0 = M['m00']
+        
+        if moment_0 == 0:
+            moment_0 = 1
+        cx = int(M['m10'] / moment_0)
+        cy = int(M['m01'] / moment_0)
+
+        cv2.circle(img, (cx, cy), 1, (0, 0, 255), 3)
+
+        cv2.imshow('image', img)
+        # Wait longer to prevent freeze for videos.
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    video_cap.release()
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import cv2
 import numpy as np
-
+import json
 from picamera2 import Picamera2
 
 def nothing(x):
@@ -13,30 +10,14 @@ def nothing(x):
 picam2 = Picamera2()
 picam2.start()
 
-
-
 cv2.namedWindow('image')
 
-# # create trackbars for color change
-cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
-cv2.createTrackbar('SMin','image',0,255,nothing)
-cv2.createTrackbar('VMin','image',0,255,nothing)
-cv2.createTrackbar('HMax','image',0,179,nothing)
-cv2.createTrackbar('SMax','image',0,255,nothing)
-cv2.createTrackbar('VMax','image',0,255,nothing)
 
-# Set default value for MAX HSV trackbars.
-cv2.setTrackbarPos('HMax', 'image', 179)
-cv2.setTrackbarPos('SMax', 'image', 255)
-cv2.setTrackbarPos('VMax', 'image', 255)
-
-# Initialize to check if HSV min/max value changes
-hMin = sMin = vMin = hMax = sMax = vMax = 0
-phMin = psMin = pvMin = phMax = psMax = pvMax = 0
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-        
+
+    with open('cameraParameters.json', 'r') as file:
+        data = json.load(file)
+  
     prev_vals = []
     prev = 0
     counter = 0
@@ -45,20 +26,12 @@ if __name__ == '__main__':
         img = picam2.capture_array()
         img = img[240:,:]
 
-        # # get current positions of all trackbars
-        hMin = cv2.getTrackbarPos('HMin', 'image')
-        sMin = cv2.getTrackbarPos('SMin', 'image')
-        vMin = cv2.getTrackbarPos('VMin', 'image')
-
-        hMax = cv2.getTrackbarPos('HMax', 'image')
-        sMax = cv2.getTrackbarPos('SMax', 'image')
-        vMax = cv2.getTrackbarPos('VMax', 'image')
-
         # Set minimum and max HSV values to display
         #lower = np.array([30, 44, 202])
         #upper = np.array([57, 171, 255])
-        lower = np.array([hMin, sMin, vMin])
-        upper = np.array([hMax, sMax, vMax])
+
+        lower = np.array([data['hMin'], data['sMin'], data['vMin']])
+        upper = np.array([data['hMax'], data['sMax'], data['vMax']])
 
         # Create HSV Image and threshold into a range.
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -97,5 +70,3 @@ if __name__ == '__main__':
 
     video_cap.release()
     cv2.destroyAllWindows()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
