c296b4ec738b4d86781ce84ddc10207d38b4287d
George Yazji <76963708+george536@users.noreply.github.com>
2023-03-14 01:04:39 -0400

    pid implementation

--- CameraLaneDetection/CamLaneTracking.py
+++ CameraLaneDetection/CamLaneTracking.py
@@ -6,9 +6,30 @@ class CamLaneTracking:
         self.ultrasonicManager = ultrasonicManager
         self.egoCar = egoCar
         self.safeZone = 10
+
+        # PID values
+        self.kp = 0.5
+        self.ki = 0.1
+        self.kd = 0.2
+
+        self.error = 0
+        self.integral = 0
+        self.previous_error = 0
 		
     def update(self):
-            print("from centering manager "+str(DetectionData.location))
+
+            self.error = DetectionData.location
+
+            proportional =  self.kp * self.error
+            integral += self.ki * self.error
+            derivative =  self.kd * ( self.error -  self.previous_error)
+
+            output = proportional + integral + derivative
+
+            self.previous_error = self.error
+
+            print("Error by PId: "+str(self.error))
+
             if self.ultrasonicManager.getEmergencyStopState()==False:
                 speed = self.egoCar.getScaledSpeed()
                 if abs(DetectionData.location)<self.safeZone:
@@ -16,3 +37,5 @@ class CamLaneTracking:
                     #PWM.setMotorModel(-speed,-speed,-speed,-speed)
             else:
                 self.egoCar.setSpeed(0)
+
+            time.sleep(0.01)
