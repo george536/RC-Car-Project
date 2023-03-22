52e18ef97e4d75cb392db04da295539976053f7e
George Yazji <76963708+george536@users.noreply.github.com>
2023-03-21 23:29:20 -0400

    cc

--- RcCarModules/led.py
+++ RcCarModules/led.py
@@ -1,39 +1,38 @@
-with open("/dev/mem", "r+b") as f:
-    import time
-    from rpi_ws281x import *
-    # LED strip configuration:
-    LED_COUNT      = 8      # Number of LED pixels.
-    LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-    LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-    LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-    LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-    LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-    LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-    # Define functions which animate LEDs in various ways.
+import time
+from rpi_ws281x import *
+# LED strip configuration:
+LED_COUNT      = 8      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# Define functions which animate LEDs in various ways.
 
-    class Led:
-        def __init__(self):
-            # Control the sending order of color data
-            self.ORDER = "RGB"
-            # Create NeoPixel object with appropriate configuration.
-            self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS,
-                                        LED_CHANNEL)
-            # Intialize the library (must be called once before other functions).
-            self.strip.begin()
+class Led:
+    def __init__(self):
+        # Control the sending order of color data
+        self.ORDER = "RGB"
+        # Create NeoPixel object with appropriate configuration.
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS,
+                                       LED_CHANNEL)
+        # Intialize the library (must be called once before other functions).
+        self.strip.begin()
 
-        def LED_TYPR(self, order, R_G_B):
-            B = R_G_B & 255
-            G = R_G_B >> 8 & 255
-            R = R_G_B >> 16 & 255
-            Led_type = ["GRB", "GBR", "RGB", "RBG", "BRG", "BGR"]
-            color = [Color(G, R, B), Color(G, B, R), Color(R, G, B), Color(R, B, G), Color(B, R, G), Color(B, G, R)]
-            if order in Led_type:
-                return color[Led_type.index(order)]
+    def LED_TYPR(self, order, R_G_B):
+        B = R_G_B & 255
+        G = R_G_B >> 8 & 255
+        R = R_G_B >> 16 & 255
+        Led_type = ["GRB", "GBR", "RGB", "RBG", "BRG", "BGR"]
+        color = [Color(G, R, B), Color(G, B, R), Color(R, G, B), Color(R, B, G), Color(B, R, G), Color(B, G, R)]
+        if order in Led_type:
+            return color[Led_type.index(order)]
 
-        def colorWipe(self, strip, color, wait_ms=50):
-            """Wipe color across display a pixel at a time."""
-            color = self.LED_TYPR(self.ORDER, color)
-            for i in range(self.strip.numPixels()):
-                self.strip.setPixelColor(i, color)
-                self.strip.show()
-                time.sleep(wait_ms / 1000.0)
+    def colorWipe(self, strip, color, wait_ms=50):
+        """Wipe color across display a pixel at a time."""
+        color = self.LED_TYPR(self.ORDER, color)
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms / 1000.0)
