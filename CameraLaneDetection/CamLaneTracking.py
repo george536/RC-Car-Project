66bb2f66f0c76d24ed16a183716b6624d42e52e7
George Yazji <76963708+george536@users.noreply.github.com>
2023-03-29 20:47:47 -0400

    cc

--- CameraLaneDetection/CamLaneTracking.py
+++ CameraLaneDetection/CamLaneTracking.py
@@ -44,9 +44,9 @@ class CamLaneTracking:
 
             # kp equation: 0.084x+21.43
             # kd equation: 0.0538x+0.676
-            self.kp = (0.048*DetectionData.testSpeed)+20.51
+            self.kp = (0.084*DetectionData.testSpeed)+21.43
             self.ki = 0
-            self.kd = (0.044*DetectionData.testSpeed)+0.68
+            self.kd = (0.040*DetectionData.testSpeed)+0.60
             
             t = time.time()
             dt = t-self.last_time
