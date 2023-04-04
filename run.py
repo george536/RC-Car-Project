b6713b0c45dbb9c0ff6ab81aeadafedce5121991
George Yazji <76963708+george536@users.noreply.github.com>
2023-04-04 00:38:49 -0400

    added commands to RUn

--- run.py
+++ run.py
@@ -4,6 +4,21 @@ import sys
 os.system("source venv/bin/activate")
 
 running_cmd = "sudo python main.py"
+calibration_cmd = "python CameraLaneDetection/calibrate.py"
+add_cmd = "git add ."
+commit_cmd = 'git commit -m "saved Calibration/parameters"'
+push_cmd = "git push"
+
+if "-calibrate" in sys.argv:
+    os.system(calibration_cmd)
+    exit
+
+if "-save" in sys.argv:
+    os.system(add_cmd)
+    os.system(commit_cmd)
+    os.system(push_cmd)
+    exit
+
 if "-testPID" in sys.argv:
     os.system(running_cmd+" -testPID")
 else:
