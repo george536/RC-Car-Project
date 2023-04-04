adacf1fd8e45da539e177b40c6150c19314ff2b3
George Yazji <76963708+george536@users.noreply.github.com>
2023-04-03 22:59:30 -0400

    cc

--- install.py
+++ install.py
@@ -4,10 +4,11 @@ commands = [
     'sudo pip install smbus',
     'sudo pip install RPi.GPIO',
     'sudo pip install rpi_ws281x',
-    'sudo pip install paho-mqtt'
+    'sudo pip install paho-mqtt',
     'sudo pip install opencv-python',
     'sudo pip install picamera2',
-    'sudo pip install libcamera',
+    'sudo apt install -y python3-picamera2',
+    'sudo apt-get install build-essential libcap-dev'
 ]
 
 for cmd in commands:
