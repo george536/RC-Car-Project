7c0434eb57beac6b543a4abad1dcf6f11906534a
George Yazji <76963708+george536@users.noreply.github.com>
2023-04-03 22:55:46 -0400

    added missing installations from install.py

--- install.py
+++ install.py
@@ -1,7 +1,12 @@
 import os
 commands = [
-    'sudo pip install opencv-python',
+    'sudo pip install smbus',
+    'sudo pip install RPi.GPIO',
+    'sudo pip install rpi_ws281x',
     'sudo pip install paho-mqtt'
+    'sudo pip install opencv-python',
+    'sudo pip install picamera2',
+    'sudo pip install libcamera',
 ]
 
 for cmd in commands:
