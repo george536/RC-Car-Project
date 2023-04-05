6771744e94e88378d5cdee8e45106359085975f1
George Yazji <76963708+george536@users.noreply.github.com>
2023-04-05 12:36:09 -0400

    global data

--- CameraLaneDetection/calibrate.py
+++ CameraLaneDetection/calibrate.py
@@ -60,15 +60,12 @@ def main():
         lower = np.array([hMin, sMin, vMin])
         upper = np.array([hMax, sMax, vMax])
 
-        global data
-
         data['hMin'] = hMin
         data['sMin'] = sMin
         data['vMin'] = vMin
         data['hMax'] = hMax
         data['sMax'] = sMax
         data['vMax'] = vMax
-
 
         # Create HSV Image and threshold into a range.
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
